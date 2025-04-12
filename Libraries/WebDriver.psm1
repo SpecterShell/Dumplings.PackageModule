c3e53f09951d8be95e9d2ddeeac153cd580150f0
SpecterShell <56779163+SpecterShell@users.noreply.github.com>
2025-04-12 15:21:20 +0800

    Rewrite WebDriver

--- Libraries/WebDriver.psm1
+++ Libraries/WebDriver.psm1
@@ -4,8 +4,20 @@ if ($DumplingsDefaultParameterValues) { $PSDefaultParameterValues = $DumplingsDe
 # Force stop on error
 $ErrorActionPreference = 'Stop'
 
-#region WebDriver
-$WebDriverLoaded = $false
+function Get-WebDriverAssemblyPath {
+  <#
+  .SYNOPSIS
+    Get the path of the WebDriver assembly
+  #>
+  [OutputType([string])]
+  param ()
+
+  if (Test-Path -Path ($Path = Join-Path $PSScriptRoot '..' 'Assets' 'WebDriver.dll')) {
+    return (Resolve-Path -Path $Path).Path
+  } else {
+    throw 'The WebDriver assembly could not be found'
+  }
+}
 
 function Initialize-WebDriver {
   <#
@@ -13,150 +25,185 @@ function Initialize-WebDriver {
     Initialize the WebDriver module
   #>
 
-  if (-not $Script:WebDriverLoaded) {
-    if (Test-Path -Path (Join-Path $PSScriptRoot '..' 'Assets' 'WebDriver.dll')) {
-      $WebDriverPath = Join-Path $PSScriptRoot '..' 'Assets' 'WebDriver.dll' -Resolve
-    } elseif ((Test-Path -Path Variable:\DumplingsRoot) -and (Test-Path -Path (Join-Path $DumplingsRoot 'Assets' 'WebDriver.dll'))) {
-      $WebDriverPath = Join-Path $DumplingsRoot 'Assets' 'WebDriver.dll' -Resolve
-    } else {
-      throw 'The WebDriver assembly could not be found'
-    }
-
-    Add-Type -Path $WebDriverPath
-    $Script:WebDriverLoaded = $true
+  # Check if the WebDriver assembly is already loaded to prevent double loading
+  if (-not ([System.Management.Automation.PSTypeName]'OpenQA.Selenium.WebDriver').Type) {
+    Add-Type -Path (Get-WebDriverAssemblyPath)
   }
 }
 #endregion
 
-# Silently initialize WebDriver when loading the module
-try { Initialize-WebDriver } catch {}
+Initialize-WebDriver
 
 #region Edge Driver
-$EdgeDriver = $null
-$EdgeDriverLoaded = $false
+[OpenQA.Selenium.Edge.EdgeDriver]$EdgeDriver = $null
+[bool]$EdgeDriverLoaded = $false
+
+function Get-EdgeDriverExecutablePath {
+  <#
+  .SYNOPSIS
+    Get the path of the EdgeDriver executable
+  #>
+  [OutputType([string])]
+  param ()
+
+  if (Test-Path -Path Env:\EDGEWEBDRIVER) {
+    # https://github.com/actions/runner-images/blob/main/images/win/Windows2022-Readme.md
+    if (Test-Path -Path $Env:EDGEWEBDRIVER) {
+      return $Env:EDGEWEBDRIVER
+    } else {
+      throw 'The EDGEWEBDRIVER environment variable is set, but the path is invalid'
+    }
+  } elseif ($Command = Get-Command -Name 'msedgedriver.exe' -ErrorAction SilentlyContinue) {
+    return $Command.Path
+  } else {
+    throw 'Could not find msedgedriver.exe'
+  }
+}
 
 function New-EdgeDriver {
   <#
   .SYNOPSIS
-    Initialize an Edge Driver instance
+    Initialize an EdgeDriver instance
   .PARAMETER Headless
-    Initialize the Edge Driver instance in headless mode, where no window pops up
+    Initialize the EdgeDriver instance in headless mode
   #>
+  [OutputType([OpenQA.Selenium.Edge.EdgeDriver])]
   param (
-    [parameter(HelpMessage = 'Initialize the Edge Driver instance in headless mode, where no window pops up')]
-    [switch]$Headless
+    [parameter(HelpMessage = 'Initialize the Edge Driver instance in headless mode')]
+    [switch]$Headless = $false
   )
-
-  Initialize-WebDriver
 
   $EdgeOptions = [OpenQA.Selenium.Edge.EdgeOptions]::new()
 
-  # Run Edge in the background with no window popping up
+  # Run Edge in headless mode if specified
   if ($Headless) { $EdgeOptions.AddArgument('--headless=new') }
-  # Disable images downloading to speed up page loading
+
+  # Block image content to speed up page loading and reduce resource consumption
   $EdgeOptions.AddUserProfilePreference('profile.managed_default_content_settings.images', 2)
 
-  if (Test-Path -Path Env:\EDGEWEBDRIVER) {
-    # https://github.com/actions/runner-images/blob/main/images/win/Windows2022-Readme.md
-    $Script:EdgeDriver = [OpenQA.Selenium.Edge.EdgeDriver]::new($Env:EDGEWEBDRIVER, $EdgeOptions)
-  } elseif (Get-Command -Name 'msedgedriver.exe' -ErrorAction SilentlyContinue) {
-    $Script:EdgeDriver = [OpenQA.Selenium.Edge.EdgeDriver]::new((Get-Command -Name 'msedgedriver.exe').Path, $EdgeOptions)
-  } else {
-    throw 'Could not find msedgedriver.exe'
-  }
+  $EdgeDriver = [OpenQA.Selenium.Edge.EdgeDriver]::new((Get-EdgeDriverExecutablePath), $EdgeOptions)
 
-  $Script:EdgeDriverLoaded = $true
+  # Resize the window to 1920x1080 to ensure the page is not rendered in mobile layout
+  $EdgeDriver.Manage().Window.Size = [System.Drawing.Size]::new(1920, 1080)
 
-  # Resize the window to 1920x1080 to ensure the page is not rendered in mobile mode
-  $Script:EdgeDriver.Manage().Window.Size = [System.Drawing.Size]::new(1920, 1080)
-  # Block images, videos, and other media files to speed up page loading and reduce resource consumption
+  # Block image and video URLs to speed up page loading and reduce resource consumption
   $Dict = [System.Collections.Generic.Dictionary[string, object]]::new()
   $Dict.Add('urls', @('*.jpg*', '*.jpeg*', '*.bmp*', '*.png*', '*.webp*', '*.gif*', '*.svg*', '*.mp4*', '*.webm*', '*.flv*'))
-  $null = $Script:EdgeDriver.ExecuteCdpCommand('Network.setBlockedURLs', $Dict)
+  $null = $EdgeDriver.ExecuteCdpCommand('Network.setBlockedURLs', $Dict)
+
+  # Enable network interception
   $Dict = [System.Collections.Generic.Dictionary[string, object]]::new()
-  $null = $Script:EdgeDriver.ExecuteCdpCommand('Network.enable', $Dict)
+  $null = $EdgeDriver.ExecuteCdpCommand('Network.enable', $Dict)
+
+  return $EdgeDriver
 }
 
 function Get-EdgeDriver {
   <#
   .SYNOPSIS
-    Return the Edge Driver instance
+    Initialize and return the managed EdgeDriver instance
   #>
   [OutputType([OpenQA.Selenium.Edge.EdgeDriver])]
   param ()
 
-  if (-not $Script:EdgeDriverLoaded) { New-EdgeDriver @args }
+  if (-not $Script:EdgeDriverLoaded) {
+    $Script:EdgeDriver = New-EdgeDriver @args
+    $Script:EdgeDriverLoaded = $true
+  }
   return $Script:EdgeDriver
 }
 
 function Stop-EdgeDriver {
   <#
   .SYNOPSIS
-    Stop the Edge Driver instance
+    Stop and dispose the managed EdgeDriver instance
   #>
 
   if ($Script:EdgeDriverLoaded) {
-    $Script:EdgeDriver.Quit()
+    $Script:EdgeDriver.Dispose()
     $Script:EdgeDriverLoaded = $false
   }
 }
 #endregion
 
 #region FirefoxDriver
-$FirefoxDriver = $null
-$FirefoxDriverLoaded = $false
+[OpenQA.Selenium.Firefox.FirefoxDriver]$FirefoxDriver = $null
+[bool]$FirefoxDriverLoaded = $false
+
+function Get-FirefoxDriverExecutablePath {
+  <#
+  .SYNOPSIS
+    Get the path of the FirefoxDriver executable
+  #>
+  [OutputType([string])]
+  param ()
+
+  if (Test-Path -Path Env:\GECKOWEBDRIVER) {
+    # https://github.com/actions/runner-images/blob/main/images/win/Windows2022-Readme.md
+    if (Test-Path -Path $Env:GECKOWEBDRIVER) {
+      return $Env:GECKOWEBDRIVER
+    } else {
+      throw 'The GECKOWEBDRIVER environment variable is set, but the path is invalid'
+    }
+  } elseif ($Command = Get-Command -Name 'geckodriver.exe' -ErrorAction SilentlyContinue) {
+    return $Command.Path
+  } else {
+    throw 'Could not find msedgedriver.exe'
+  }
+}
 
 function New-FirefoxDriver {
   <#
   .SYNOPSIS
-    Initialize an Firefox Driver instance
+    Initialize an FirefoxDriver instance
   .PARAMETER Headless
-    Initialize the Firefox Driver instance in headless mode, where no window pops up
+    Initialize the FirefoxDriver instance in headless mode
   #>
+  [OutputType([OpenQA.Selenium.Firefox.FirefoxDriver])]
   param (
-    [parameter(HelpMessage = 'Initialize the Firefox Driver instance in headless mode, where no window pops up')]
+    [parameter(HelpMessage = 'Initialize the FirefoxDriver instance in headless mode')]
     [switch]$Headless
   )
 
-  Initialize-WebDriver
-
   $FirefoxOptions = [OpenQA.Selenium.Firefox.FirefoxOptions]::new()
-  # Disable images downloading to speed up page loading
-  $FirefoxOptions.SetPreference('permissions.default.image', 2)
+
+  # Run Firefox in headless mode if specified
   if ($Headless) { $FirefoxOptions.AddArgument('--headless') }
 
-  if (Test-Path Env:\GECKOWEBDRIVER) {
-    # https://github.com/actions/runner-images/blob/main/images/win/Windows2022-Readme.md
-    $Script:FirefoxDriver = [OpenQA.Selenium.Firefox.FirefoxDriver]::new($Env:GECKOWEBDRIVER, $FirefoxOptions)
-  } elseif (Get-Command -Name 'geckodriver.exe' -ErrorAction SilentlyContinue) {
-    $Script:FirefoxDriver = [OpenQA.Selenium.Firefox.FirefoxDriver]::new((Get-Command -Name 'geckodriver.exe').Path, $FirefoxOptions)
-  } else {
-    throw 'Could not find geckodriver.exe'
-  }
-  $Script:FirefoxDriver.Manage().Window.Size = [System.Drawing.Size]::new(1920, 1080)
-  $Script:FirefoxDriverLoaded = $true
+  # Block image content to speed up page loading and reduce resource consumption
+  $FirefoxOptions.SetPreference('permissions.default.image', 2)
+
+  $FirefoxDriver = [OpenQA.Selenium.Firefox.FirefoxDriver]::new((Get-FirefoxDriverExecutablePath), $FirefoxOptions)
+
+  # Resize the window to 1920x1080 to ensure the page is not rendered in mobile layout
+  $FirefoxDriver.Manage().Window.Size = [System.Drawing.Size]::new(1920, 1080)
+
+  return $FirefoxDriver
 }
 
 function Get-FirefoxDriver {
   <#
   .SYNOPSIS
-    Return the Firefox Driver instance
+    Initialize and return the managed FirefoxDriver instance
   #>
   [OutputType([OpenQA.Selenium.Firefox.FirefoxDriver])]
   param ()
 
-  if (-not $Script:FirefoxDriverLoaded) { New-FirefoxDriver @args }
+  if (-not $Script:FirefoxDriverLoaded) {
+    $Script:FirefoxDriver = New-FirefoxDriver @args
+    $Script:FirefoxDriverLoaded = $true
+  }
   return $Script:FirefoxDriver
 }
 
 function Stop-FirefoxDriver {
   <#
   .SYNOPSIS
-    Stop the Firefox Driver instance
+    Stop and dispose the managed FirefoxDriver instance
   #>
 
   if ($Script:FirefoxDriverLoaded) {
-    $Script:FirefoxDriver.Quit()
+    $Script:FirefoxDriver.Dispose()
     $Script:FirefoxDriverLoaded = $false
   }
 }
